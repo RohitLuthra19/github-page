adcfcd6a82d64cf0a266883dc0967729af277f1f
Rohit Luthra <rohit.luthra19@gmail.com>
2020-07-26 16:28:35 +0530

    fixed spinner and corrected attribute name

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
 
 import "./App.css";
-import SideNav from '../SideNav/SideNav';
-import Main from '../Main/Main';
+import SideNav from "../SideNav/SideNav";
+import Main from "../Main/Main";
+import Spinner from "../../components/Spinner/Spinner";
 
-const App = props => (
-  <div className="row">
-    <SideNav />
-    <div className="main">
-      <Main/>
-    </div>
-  </div>
-);
+const App = () => {
+  const user = useSelector(state => state.user);
+  const { user: {fetching, error}} = user.toJS();
+
+  if(error) 
+    return "error in loading page"
+
+  return (
+    <>
+      <div className="row">
+        <SideNav />
+        <div className="main">
+          <Main />
+        </div>
+      </div>}
+      <Spinner left={"60%"} top={"60%"} fontSize={"3em"} visible={fetching} />
+    </>
+  )
+};
 
 export default App;
--- src/routes/Main/Main.jsx
+++ src/routes/Main/Main.jsx
@@ -2,24 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import "./Main.css";
-import Spinner from '../../components/Spinner/Spinner';
 
 import { getRepositories } from '../../redux/user/reducer';
 
 export class Main extends React.PureComponent {
     render() {
-      const { repos, fetching } = this.props;
+      const { repos } = this.props;
       console.log(repos)
       return (
         <div className="main">
           <div className="wrapper">
             {this.renderImages(repos)}
-            <Spinner
-              left={'60%'}
-              top={'60%'}
-              fontSize={'3em'}
-              visible={fetching}
-            />
+            
           </div>
         </div>
       );
