e7c0b3ffcae078b03ee3c300c34c92615ab462a5
Rohit Luthra <rohit.luthra19@gmail.com>
2020-07-26 15:53:40 +0530

    update snapshots

--- src/routes/SideNav/SideNav.jsx
+++ src/routes/SideNav/SideNav.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 
-
-import PeopleIcon from '../../assets/people-icon';
-import KebabHorizontalIcon from '../../assets/kebab-horizontal-icon'
-import StarIcon from '../../assets/star-icon'
-import OrganizationIcon from '../../assets/organization-icon'
-import LocationIcon from '../../assets/location-icon'
-import EmailIcon from '../../assets/email-icon'
+import PeopleIcon from "../../assets/people-icon";
+import KebabHorizontalIcon from "../../assets/kebab-horizontal-icon";
+import StarIcon from "../../assets/star-icon";
+import OrganizationIcon from "../../assets/organization-icon";
+import LocationIcon from "../../assets/location-icon";
+import EmailIcon from "../../assets/email-icon";
 import "./SideNav.css";
 
 import { getUserProfile } from "../../redux/user/reducer";
@@ -15,12 +14,28 @@ import { getUserProfile } from "../../redux/user/reducer";
 export class SideNav extends React.PureComponent {
   render() {
     const {
-      profile: { name, login, avatar_url , bio, followers, following, company, location, email},
+      profile: {
+        name,
+        login,
+        avatar_url,
+        bio,
+        followers,
+        following,
+        company,
+        location,
+        email,
+      },
     } = this.props;
 
     return (
       <div className="side">
-        <img className="user" alt="" width="260" height="260" src={avatar_url}/>
+        <img
+          className="user"
+          alt=""
+          width="260"
+          height="260"
+          src={avatar_url}
+        />
 
         <h1 className="profile">
           <span className="fullname">{name}</span>
@@ -30,19 +45,39 @@ export class SideNav extends React.PureComponent {
         {bio && <div>{bio}</div>}
         <div className="btn-group">
           <button className="btn">Follow</button>
-          <button className="report-btn"><KebabHorizontalIcon /></button>
+          <button className="report-btn">
+            <KebabHorizontalIcon />
+          </button>
         </div>
 
         <div className="follower-following">
-          <PeopleIcon /> <span className="label">{followers}</span> followers <span className="label middle"></span> <span className="label">{following}</span> following <span className="label middle"></span> <StarIcon /> <span className="label">7</span>
+          <PeopleIcon /> <span className="label">{followers}</span> followers{" "}
+          <span className="label middle"></span>{" "}
+          <span className="label">{following}</span> following{" "}
+          <span className="label middle"></span> <StarIcon />{" "}
+          <span className="label">7</span>
         </div>
 
         <ul className="other-details">
-          {company && <li><OrganizationIcon/><span className="list-item">{company}</span></li>}
-          {location && <li><LocationIcon/><span className="list-item">{location}</span></li>}
-          {email && <li><EmailIcon/><span className="list-item">{email}</span></li>}
+          {company && (
+            <li className="list-item">
+              <OrganizationIcon />
+              <span className="list-item-text">{company}</span>
+            </li>
+          )}
+          {location && (
+            <li className="list-item">
+              <LocationIcon />
+              <span className="list-item-text">{location}</span>
+            </li>
+          )}
+          {email && (
+            <li className="list-item">
+              <EmailIcon />
+              <span className="list-item-text">{email}</span>
+            </li>
+          )}
         </ul>
-
       </div>
     );
   }
--- src/routes/SideNav/SideNav.spec.js
+++ src/routes/SideNav/SideNav.spec.js
@@ -3,7 +3,7 @@ import SideNav from './SideNav';
 import '../../setupTests';
 import renderer from 'react-test-renderer';
 import store from '../../redux/store';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 
 describe('SideNav renders',() => {
@@ -18,16 +18,4 @@ describe('SideNav renders',() => {
     );
     expect(rendered.toJSON()).toMatchSnapshot();
   })
-
-  it('find text:Categories', () => {
-    const wrapper = mount(<SideNav store={store} />);
-    const text = wrapper.find('h2').text();
-    expect(text).toEqual('Categories');
-  });
-
-  it('side nav bar ul count', () => {
-    const wrapper = mount(<SideNav store={store} />);
-    const navBarLink = wrapper.find('ul');
-    expect(navBarLink.length).toEqual(1);
-  });
 })
